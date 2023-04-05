6b67007a96f159adf92d6b44761c1712c4e8d255
mariama4 <mariama4@mail.ru>
2023-04-05 23:43:20 +0700

    add api

---
+++ apps/api/src/app/controllers/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { JWTAuthGuard } from '../guards/jwt.guard';
+import { UserId } from '../guards/user.decorator';
+
+@Controller('user')
+export class UserController {
+	constructor() {
+		return;
+	}
+
+	@UseGuards(JWTAuthGuard)
+	@Post('info')
+	async register(@UserId() userId: string) {
+		return { id: userId };
+	}
+}
